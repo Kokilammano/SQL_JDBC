eec46e266bba9b0cac13efcbfc358fd650f0a91b
Kokilammano <80097708+Kokilammano@users.noreply.github.com>
2021-10-27 16:29:12 +0530

    Create ProductBean.java

---
+++ ProductBean.java
@@ -0,0 +1,29 @@
+package assignment_7;
+
+//Small e-commerce model
+
+public class ProductBean {
+	private int product_id;
+	private String name;
+	private int price;
+	
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+}
